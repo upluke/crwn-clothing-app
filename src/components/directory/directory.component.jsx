309dc527f55b1e55c4ff24bd666b448a07a6dd33
upluke <lukeforadream@gmail.com>
2022-07-07 23:58:26 -0700

    directory css

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,8 +1,9 @@
 import CategoryItem from "../category-item/category-item.component"
+import './directory.styles.scss'
 
 const Directory=({categories})=>{
     return(
-        <div className="categories-container">
+        <div className="directory-container">
         {categories.map((category)=>(
           <CategoryItem key={category.id} category={category}/>
         
