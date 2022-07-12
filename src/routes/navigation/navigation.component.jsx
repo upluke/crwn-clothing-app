f7a70a88b2e139b6f42ddf2c207338efa04b32e1
upluke <lukeforadream@gmail.com>
2022-07-11 23:18:51 -0700

    routes for home and shop page

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,20 @@
-import {Outlet} from 'react-router-dom'
+import { Fragment } from 'react'
+import {Outlet, Link} from 'react-router-dom'
 
 
 const Navigation=()=>{
     return(
-        <div>
-            <div>
-                <h1>Here's navigation</h1>
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to='/'>
+                    <div>Logo</div> 
+                </Link>
+               <div className='nav-links-container'>
+                    <Link className='nav-link' to='/shop'>SHOP</Link>
+               </div>
             </div>
             <Outlet />
-        </div>
+        </Fragment>
     )
 }
 
