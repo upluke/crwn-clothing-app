8e23039042eb2a5eab2f21c31a2afb0074a57adb
upluke <lukeforadream@gmail.com>
2022-07-11 23:10:22 -0700

    add navigation page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import {Routes, Route} from 'react-router-dom'
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component'
+
+const Shop =()=>{
+  return <h1>This is the shop page!</h1>
+}
 
 function App() {
  
   
   return (
     <Routes>
-      <Route path='/' element={ <Home/>} />
+      <Route path='/' element={<Navigation />} >
+        <Route index element={ <Home/>} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
    
   );
