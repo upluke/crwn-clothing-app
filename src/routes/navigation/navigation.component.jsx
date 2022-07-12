19c5cfa0ee2fd716db3c5ff83c770f93c18e0351
upluke <lukeforadream@gmail.com>
2022-07-12 10:07:13 -0700

    add svg and box-sizing

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,16 @@
 import { Fragment } from 'react'
 import {Outlet, Link} from 'react-router-dom'
-
+// SVGs can be imported and used directly as a React component in your React code. The image is not loaded as a separate file, instead, it’s rendered along the HTML.
+// Note this approach only works with create-react-app. If you are not using create-react-app, I would recommend using other approaches. Create-react-app uses SVGR under the hood to make it possible to transform and import SVG as a React component.
+import {ReactComponent as CrownLogo} from '../../assets/crown.svg'
+import './navigation.styles.scss'
 
 const Navigation=()=>{
     return(
         <Fragment>
             <div className='navigation'>
                 <Link className='logo-container' to='/'>
-                    <div>Logo</div> 
+                    <CrownLogo className='logo' />
                 </Link>
                <div className='nav-links-container'>
                     <Link className='nav-link' to='/shop'>SHOP</Link>
