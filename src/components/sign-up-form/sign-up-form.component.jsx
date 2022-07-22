06f7c475050212d46e0a841949afef6d9236b59a
upluke <lukeforadream@gmail.com>
2022-07-21 19:24:36 -0700

    renamed variables

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,23 +1,26 @@
 import { useState } from "react"
 
-const SignUpForm =()=>{
-    const initData={
-        email:'',
-        password:'',
-        password_confirmed:''
-    }
+const defaultForFields={
+    displayName:'',
+    email:'',
+    password:'',
+    confirmPassword:''
+}
 
-    const [inputItem, setInputItem] =useState(initData)
-    const {email, password, password_confirmed} = inputItem
+const SignUpForm =()=>{
+
+
+    const [formFields, setFormFields] =useState(defaultForFields)
+    const {displayName, email, password, confirmPassword} = formFields
 
     const handleChange=(event)=>{
-        const {name, value} = event.target
-        setInputItem({...inputItem, [name]:value})
+        const {name, value} = event.target 
+        setFormFields({...formFields, [name]:value})
     }
 
     const handleSubmit=(event)=>{
         event.preventDefault()
-        console.log(email,"---",password,"----", password_confirmed)
+        console.log(displayName,"---",email, "------",password,"----")
     }
 
     
@@ -25,13 +28,15 @@ const SignUpForm =()=>{
         <div>
             <h1>Sign up with your email and password</h1>
             <form onSubmit={handleSubmit}>
-                <label>Email: </label>
-                <input type="email" required name="email" onChange={handleChange} />
-                <label>password: </label>
-                <input type="password" required name="password" autoComplete="on" onChange={handleChange} />
-                <label>comfirmed password: </label>
-                <input type="password" required name="password_confirmed" autoComplete="on" onChange={handleChange} />
-                <button >Submit</button>
+                <label>Display Name</label>
+                <input type="text" required onChange={handleChange} name="displayName" value={displayName}/>
+                <label>Email </label>
+                <input type="email" required onChange={handleChange} name="email" value={email}/>
+                <label>Password: </label>
+                <input type="password" required  autoComplete="on" onChange={handleChange} name="password" value={password}/>
+                <label>Comfirm Password</label>
+                <input type="password" required  autoComplete="on" onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
