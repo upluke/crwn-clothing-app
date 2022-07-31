df8b868211f01714cfa29ce62cd2276130834a15
upluke <lukeforadream@gmail.com>
2022-07-30 18:32:23 -0700

    form-input css

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,13 +1,21 @@
+import './form-input.styles.scss'
 const FormInput=({label, ...otherProps})=>{
     return(
-        <div>
+        <div className='group'>
+             <input className="form-input" {...otherProps}/> 
             {label&&(
                 <label className={`${otherProps.value.length? 'shrink': ''} form-input-label`}>{label}</label>
             )}
-            <input className="form-input" {...otherProps}/>
+           
         </div>
     )
 
 }
 
 export default FormInput;
+
+
+// in order to make the titles of the fileds to move up when we start selecting (focus), we place <input> tag above the <label>
+// In scss file, line 31, there's a "~" selector which helps find the next sibling or 
+// a subsequent sibling attached that shrinkLabel, so it's looking for a general sibling component with 'form-input-label' 
+// class after the input.
