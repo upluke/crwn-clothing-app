7acde2f0edc9a026c1616cd0015c5fca9a9ce691
upluke <lukeforadream@gmail.com>
2022-08-25 00:28:39 -0700

    cart-icon

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,6 +3,9 @@ import {Outlet, Link} from 'react-router-dom'
 // SVGs can be imported and used directly as a React component in your React code. The image is not loaded as a separate file, instead, it’s rendered along the HTML.
 // Note this approach only works with create-react-app. If you are not using create-react-app, I would recommend using other approaches. Create-react-app uses SVGR under the hood to make it possible to transform and import SVG as a React component.
 import {ReactComponent as CrownLogo} from '../../assets/crown.svg'
+
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+
 import { UserContext } from '../../contexts/user.context'
 
 import { signOutUser } from '../../utils/firebase/filebase.utils'
@@ -28,7 +31,7 @@ const Navigation=()=>{
                     ):(
                         <Link className='nav-link' to='/auth' >Sign-in</Link>
                     )}
-                    
+                <CartIcon />
                </div>
             </div>
             <Outlet />
