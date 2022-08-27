fc6fe73714ff0f3074ed69454bc96a924a360c7e
upluke <lukeforadream@gmail.com>
2022-08-27 15:30:01 -0700

    toggle cart open

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,8 +8,12 @@ import './cart-icon.styles.scss'
 
 
 const CartIcon=()=>{
+    const {isCartOpen, setIsCartOpen} =useContext(CartContext)
+
+    const toggleIsCartOpen =()=> setIsCartOpen(!isCartOpen)
+
     return(
-        <div className='cart-icon-container'>
+        <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon'/>
             <span className='item-count'>0</span>
         </div>
