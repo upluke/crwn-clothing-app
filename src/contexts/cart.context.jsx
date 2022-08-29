c5a082de1d9f7c59cdc64f75962bfe29c4f589f9
upluke <lukeforadream@gmail.com>
2022-08-28 23:10:25 -0700

    carContext

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,30 @@
 import {createContext, useState} from 'react'
 
+
+const addCartItem =(cartItems, productToAdd)=>{
+    // find if cartItems contains productToAdd
+
+    // If found, increment quantity
+
+    // return new array with modified cartItems/ new cart item
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen:()=>{}
+    setIsCartOpen:()=>{},
+    cartItems:[],
+    addItemToCart:()=>{}
 })
 
 
 export const CartProvider =({children})=>{
     const [isCartOpen, setIsCartOpen] =useState(false)
+    const [cartItems, setCartItems] =useState([])
+
+    const addItemToCart =(productToAdd) =>{
+        setCartItems(addCartItem(cartItems, productToAdd))
+    }
+
     const value ={isCartOpen, setIsCartOpen}
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
