c9ceab099474042b683dae8f10fe67ff91a1320b
upluke <lukeforadream@gmail.com>
2022-09-02 20:42:16 -0700

    addcartitem

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -6,7 +6,12 @@ const addCartItem =(cartItems, productToAdd)=>{
     // find if cartItems contains productToAdd
    const existingCartItem=cartItems.find((cartItem)=>cartItem.id===productToAdd.id)
     // If found, increment quantity
-
+    if (existingCartItem){
+        return cartItems.map((cartItem)=>
+            cartItem.id===productToAdd.id
+            ?{...cartItem, quantity: cartItem.quantity+1} : cartItem
+        )
+    }
     // return new array with modified cartItems/ new cart item
     return [...cartItems, {...productToAdd, quantity:1}]
 }
